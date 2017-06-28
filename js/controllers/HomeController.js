a092cabb844b0b23540b0e058b74b96f97eedd81
Elias Escobar <elias@viaro.net>
2017-06-28 15:23:31 -0600

    show the list of json in the ng-view

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,16 @@
-app.controller('HomeController', ['$scope', function($scope) {
-$scope.helloWorld = "Hello, AngularJS!";
+app.controller('HomeController', ['$scope','suggestions', function($scope, suggestions) {
+	$scope.posts= suggestions.posts
+	$scope.addSuggestion = function() {
+		if(!$scope.title || $scope.title === "") {
+			return;
+		}
+		$scope.posts.push({
+			title: $scope.title,
+			upvotes: 0,
+		});
+		$scope.title = '';
+	};
+	$scope.upVote =function(x) {
+    	x.upvotes +=1;
+  };
 }]);
